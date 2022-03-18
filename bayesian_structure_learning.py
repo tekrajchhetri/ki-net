f07c5f8e55def0b5d581c58cfa594689145b82cc
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-03-18 16:15:21 +0100

    linear structure learning

--- bayesian_structure_learning.py
+++ bayesian_structure_learning.py
@@ -5,3 +5,9 @@
 # @Web     : https://tekrajchhetri.com/
 # @File    : bayesian_structure_learning.py
 # @Software: PyCharm
+
+from causalnex.structure.pytorch.notears import from_pandas
+
+def start_linear_structure_learning(dataset):
+    sm = from_pandas(dataset)
+    return sm
